04cc421a693ccc3eec2fb786f526cd093688356e
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-23 00:56:46 +0100

    Configuration Management - Tasks 1 bug fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Using Puppet, install_flask.pp from pip3
+# Ensure Python3 and pip3 are installed
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
