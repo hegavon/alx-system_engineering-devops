c264e288ba80a59d91f9ef685e66810e3995a424
Victor Amajuoyi <victorgavon@gmail.com>
2024-03-23 00:53:26 +0100

    Configuration Management - Tasks 1 bug fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Define a package resource for installing Flask
-package { 'Flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
