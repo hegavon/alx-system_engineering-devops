9d0289f4c21e0b678e6b8e892503b4a6b1eb68be
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-22 16:41:21 +0100

    0x15-Api - Task 3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-"""Gathers employee data from API and exports it in JSON format."""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-    todos = requests.get(url + "todos").json()
 
-    all_employee_data = {}
-
-    for user in users:
-        user_id = user["id"]
-        username = user["username"]
-
-        user_tasks = []
-        for todo in todos:
-            if todo["userId"] == user_id:
-                user_tasks.append({
-                    "username": username,
-                    "task": todo["title"],
-                    "completed": todo["completed"]
-                })
-
-        all_employee_data[user_id] = user_tasks
-
-    output_file = "todo_all_employees.json"
-
-    with open(output_file, "w") as file:
-        json.dump(all_employee_data, file)
-
-    print("Data exported to {} successfully.".format(output_file))
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            "user_id": [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
