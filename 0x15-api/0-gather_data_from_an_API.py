a8dec004d5e752646d1c785898a8fd5410eeeaeb
Victor Amajuoyi <victorgavon@gmail.com>
2024-04-22 16:14:23 +0100

    0x15-Api - Task 0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
-"""Gathers employee ID from database"""
+"""Gathers employee ID information from database."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py [employee_id]")
+        sys.exit(1)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+
+    user_response = requests.get(base_url + "users/{}".format(employee_id))
+    if user_response.status_code != 200:
+        print("Error: Employee not found.")
+        sys.exit(1)
+
+    todos_response = requests.get(
+        base_url + "todos", params={"userId": employee_id})
+    if todos_response.status_code != 200:
+        print("Error: Unable to retrieve employee's tasks.")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
+    completed_tasks = [task for task in todos_data if task.get("completed")]
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        user_data.get("name"), len(completed_tasks), len(todos_data)))
+
+    if completed_tasks:
+        for task in completed_tasks:
+            print("\t{}".format(task.get("title")))
