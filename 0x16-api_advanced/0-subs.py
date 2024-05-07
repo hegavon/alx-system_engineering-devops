f219b947f82ce7eb6a69afea029f3b56db9dd5fb
Victor Amajuoyi <victorgavon@gmail.com>
2024-05-07 22:08:39 +0100

    Advanced API - Tasks update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
+#!/usr/bin/env python3
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """
+    Queries the Reddit API returns subscribers for a given subreddit
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        Number of subscribers for subreddit If subreddit is invalid return 0
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Gavon/1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        data = response.json().get("data")
+        if data:
+            return data.get("subscribers", 0)
+    return 0
